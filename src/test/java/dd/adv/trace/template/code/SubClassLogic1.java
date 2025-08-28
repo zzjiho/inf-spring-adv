b4eccc58cd91266e83d4892bddabbbc8b2081d2f
zzjiho <youngyoungnim@gmail.com>
2025-08-29 02:02:58 +0900

    템플릿 메소드 패턴 예제

---
+++ src/test/java/dd/adv/trace/template/code/SubClassLogic1.java
@@ -0,0 +1,11 @@
+package dd.adv.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SubClassLogic1 extends AbstractTemplate {
+    @Override
+    protected void call() {
+        log.info("비즈니스 로직1 실행");
+    }
+}
